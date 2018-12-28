7a25cd26604d733eecfd9f5b1ced07d556894bed
Jared Avila <jared.c.avila@gmail.com>
2018-12-28 10:53:41 -0800

    fixing probs

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // DB config
-const db = require("./config/keys_dev.js").mongoURI;
+const db = require("./config/keys").mongoURI;
 
 // connect to DB
 mongoose
