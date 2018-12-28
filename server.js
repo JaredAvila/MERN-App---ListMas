0b223afb51d2655043dc1814a8954c365f575417
Jared Avila <jared.c.avila@gmail.com>
2018-12-28 12:08:50 -0800

    fixing deployment issues

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use("/profile", profile);
 //Server static assets if in production
 if (process.env.NODE_ENV === "production") {
   //set static folder
-  app.use(epxress.static("client/build"));
+  app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
