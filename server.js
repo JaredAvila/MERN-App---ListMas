43877dd7313e020f33f8e04ecdaa049888683c16
Jared Avila <jared.c.avila@gmail.com>
2018-12-28 11:03:48 -0800

    working on delployment

--- server.js
+++ server.js
@@ -46,6 +46,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 app.listen(port, () => console.log("running on port: ", port));
